055174a8cbe60d4a8d67107b6a7fc1b04072d5d8
unknown <Jeet1804>
2025-08-09 18:20:24 +0530

    this is my first commit in bath29

--- Script1.sh
+++ Script1.sh
@@ -1,4 +1,4 @@
 echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
-echo "We are from batch-27 and learning DevOps"
-echo "This is a test script for our DevOps training"
+echo "This is a test script for learning purposes"
+echo "This script is located at: Scripting/Script1.sh"
